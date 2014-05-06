135d712cb323b97998e2bea8c913978d75c187a3
Florian HENRY <florian.henry@open-conept.pro>
2014-05-06 19:48:57 +0200

    Add translation for month and day and fix Goto date

--- resource_planning.php
+++ resource_planning.php
@@ -33,6 +33,7 @@ if (! $res) die("Include of main fails");
 // Translations
 $langs->load("companies");
 $langs->load("other");
+$langs->load("resource@resource");
 
 //FIXME: missing rights enforcement
 
@@ -51,6 +52,45 @@ $morejs=array(
 
 $title = $langs->trans('ResourcePlaning');
 
+$monthNames=array(	'"'.$langs->trans('Month01').'"',
+				  	'"'.$langs->trans('Month02').'"',
+				 	'"'.$langs->trans('Month03').'"',
+					'"'.$langs->trans('Month04').'"',
+					'"'.$langs->trans('Month05').'"',
+					'"'.$langs->trans('Month06').'"',
+					'"'.$langs->trans('Month07').'"',
+					'"'.$langs->trans('Month08').'"',
+					'"'.$langs->trans('Month09').'"',
+					'"'.$langs->trans('Month10').'"',
+					'"'.$langs->trans('Month11').'"',
+					'"'.$langs->trans('Month12').'"');
+$monthNamesShort=array(	'"'.$langs->trans('MonthShort01').'"',
+		'"'.$langs->trans('MonthShort02').'"',
+		'"'.$langs->trans('MonthShort03').'"',
+		'"'.$langs->trans('MonthShort04').'"',
+		'"'.$langs->trans('MonthShort05').'"',
+		'"'.$langs->trans('MonthShort06').'"',
+		'"'.$langs->trans('MonthShort07').'"',
+		'"'.$langs->trans('MonthShort08').'"',
+		'"'.$langs->trans('MonthShort09').'"',
+		'"'.$langs->trans('MonthShort10').'"',
+		'"'.$langs->trans('MonthShort11').'"',
+		'"'.$langs->trans('MonthShort12').'"');
+$dayNames=array(	'"'.$langs->trans('Monday').'"',
+		'"'.$langs->trans('Tuesday').'"',
+		'"'.$langs->trans('Wednesday').'"',
+		'"'.$langs->trans('Thursday').'"',
+		'"'.$langs->trans('Friday').'"',
+		'"'.$langs->trans('Saturday').'"',
+		'"'.$langs->trans('Sunday').'"');
+$dayNamesShort=array(	'"'.$langs->trans('MondayMin').'"',
+		'"'.$langs->trans('TuesdayMin').'"',
+		'"'.$langs->trans('WednesdayMin').'"',
+		'"'.$langs->trans('ThursdayMin').'"',
+		'"'.$langs->trans('FridayMin').'"',
+		'"'.$langs->trans('SaturdayMin').'"',
+		'"'.$langs->trans('SundayMin').'"');
+
 $fullcalendar = '<script type="text/javascript" language="javascript">
 jQuery(document).ready(function() {
 	$("#calendar").fullCalendar({
@@ -58,6 +98,18 @@ jQuery(document).ready(function() {
 			left: "prev,next today",
 			center: "title",
 			right: "resourceDay,resourceWeek,resourceNextWeeks,resourceMonth"
+		},
+		monthNames: ['.implode(',',$monthNames).'],
+		monthNamesShort: ['.implode(',',$monthNamesShort).'],
+		dayNames: ['.implode(',',$dayNames).'],
+		dayNamesShort: ['.implode(',',$dayNamesShort).'],
+		buttonText: {
+		largePrev: "<span class=\'fc-text-arrow\'>&laquo;</span>",
+		largeNext: "<span class=\'fc-text-arrow\'>&raquo;</span>",
+		resourceDay: \''.$langs->trans('ByDay').'\',
+		resourceWeek: \''.$langs->trans('ByWeek').'\',
+		resourceNextWeeks: \''.$langs->trans('ByNextWeek').'\',
+		resourceMonth: \''.$langs->trans('ByMonth').'\'
 		},
 		defaultView: "resourceWeek",
 		resources: "' . dol_buildpath('/resource/core/ajax/resource_action.json.php?action=resource', 1) . '",
@@ -74,10 +126,23 @@ jQuery(document).ready(function() {
 			});
 		}
 	});
+				
+	// Click Function
+	$(":button[name=gotodate]").click(function() {
+		day=$("#select_start_dateday").val();
+		month=$("#select_start_datemonth").val()-1;
+		year=$("#select_start_dateyear").val();
+		datewished= new Date(year, month, day);
+		$("#calendar").fullCalendar( \'gotoDate\', datewished );
+	});	
+		
 });
 </script>';
 
 llxHeader($fullcalendar, $title, '', '', 0, 0, $morejs, $morecss);
+
+print $form->select_date($select_start_date, 'select_start_date', 0, 0, 1,'',1,1);
+print '<input type="button" value="'.$langs->trans('GotoDate').'" id="gotodate" name="gotodate">';
 
 print '<div id="calendar"></div>';
 
